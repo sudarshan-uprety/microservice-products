1cf6013d4b6151930538440958cef02a8ec5fbba
sudarshan-uprety <mr.sudarshanuprety@gmail.com>
2024-06-25 14:04:34 +0545

    fix for .env in pipeline useDotEnv

--- utils/variables.py
+++ utils/variables.py
@@ -5,5 +5,6 @@ load_dotenv()
 
 DB = os.getenv('DB')
 CognitoClientId = os.getenv('CognitoClientId')
+print('required CognitoClientId is', CognitoClientId)
 CognitoRegionName = os.getenv('CognitoRegionName')
 UserPoolID = os.getenv('UserPoolID')
