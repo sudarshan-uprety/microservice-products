1fdff8a5ea847017301eba10c2ed5560147722bb
sudarshan-uprety <mr.sudarshanuprety@gmail.com>
2024-07-09 23:27:19 +0545

    fixing create and update product API (still room for improvements)

--- models/category.py
+++ models/category.py
@@ -21,6 +21,6 @@ class Category(CommonDocument):
             "id": str(self.id),
             "name": self.name,
             "description": self.description,
-            "created_by": str(self.created_by.id) if self.created_by else None,
+            # "created_by": str(self.created_by.id) if self.created_by else None,
             "status": self.status
         }
